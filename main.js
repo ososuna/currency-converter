8afca3faaa318f026480a4e2f1f00df20cebab01
Oswaldo Adrián Osuna <oswaldo.adrian@live.com.mx>
2022-02-27 13:16:42 -0600

    FINALLY WORKSgit s

--- main.js
+++ main.js
@@ -2,76 +2,59 @@
 const currency = [
   {
     id: 'USD',
-    name: 'USD - US Dollar'
+    name: 'USD - US Dollar',
+    value: 1
   },
   {
     id: 'MXN',
-    name: 'MXN - Mexican Peso'
+    name: 'MXN - Mexican Peso',
+    value: 20.35
   },
   {
     id: 'JPY',
-    name: 'JPY - Japanese Yen'
+    name: 'JPY - Japanese Yen',
+    value: 115.55
   },
   {
     id: 'EUR',
-    name: 'EUR - Euro'
+    name: 'EUR - Euro',
+    value: 0.89
   }
 ]
 
-const fromCurrency = document.getElementById('fromCurrency'); 
-const toCurrency = document.getElementById('toCurrency'); 
+const fromCurrency    = document.getElementById('fromCurrency'); 
+const toCurrency      = document.getElementById('toCurrency'); 
+const valueToConvert  = document.getElementById('valueToConvert');
 
+let values = [];
 
 const convert = ( event ) => {
   event.preventDefault();
-  const valueToConvert = document.getElementById('valueToConvert').value;
-  console.log('Converting...');
-  console.log(valueToConvert);
+  
+  const fromValue = fromCurrency.value;
+  const toValue = toCurrency.value;
+
+  console.log('From', fromValue);
+  console.log('To', toValue);
+
 }
 
 const updateFromCurrencyOpts = () => {
   
-  const options = currency.filter( c => c.id !== fromCurrency.value );
-  const prevValue = toCurrency.value;
-  
-  toCurrency.options.length = 0;
-
-  options.forEach( opt => {
-    const option = document.createElement('option');
-    option.textContent = opt.name;
-    option.value = opt.id;
-    toCurrency.appendChild(option);
-  });
-
-  const foundValue = options.find( opt => opt.id === prevValue );
-
-  if ( foundValue ) {
-    toCurrency.value = prevValue;
+  if ( fromCurrency.value === values[1] ) {
+    toCurrency.value = values[0];
+    values[1] = toCurrency.value
   }
-
+    values[0] = fromCurrency.value;
 }
 
 const updateToCurrencyOpts = () => {
   
-  const options = currency.filter( c => c.id !== toCurrency.value );
-  const prevValue = fromCurrency.value;
-
-  fromCurrency.options.length = 0;
-  
-  options.forEach( opt => {
-    const option = document.createElement('option');
-    option.textContent = opt.name;
-    option.value = opt.id;
-    fromCurrency.appendChild(option);
-  });
-
-  const foundValue = options.find( opt => opt.id === prevValue );
-
-  if ( foundValue ) {
-    fromCurrency.value = prevValue;
+  if ( toCurrency.value === values[0] ) {
+    fromCurrency.value = values[1];
+    values[0] = fromCurrency.value
   }
-
-
+    values[1] = toCurrency.value;
 }
 
 currency.forEach( c => {
@@ -82,10 +65,13 @@ currency.forEach( c => {
 });
 
 currency.forEach( c => {
-  if ( c.id != fromCurrency.value ) {
-    const opt = document.createElement('option');
-    opt.textContent = c.name;
-    opt.value = c.id;
-    toCurrency.appendChild(opt);
-  }
+  const opt = document.createElement('option');
+  opt.textContent = c.name;
+  opt.value = c.id;
+  toCurrency.appendChild(opt);
 });
+
+
+values = [ fromCurrency.value, toCurrency.value ];
+
+console.log( values );
